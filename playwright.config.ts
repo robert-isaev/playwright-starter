25876086f0e2115bd2782dab328f85db2ed356ca
Robert Isaev <robert.isaev@gmail.com>
2021-12-17 15:09:36 +0100

    add config fiel

---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  testDir: "src/features",
+  timeout: 10000,
+  retries: 0,
+  use: {
+    headless: false,
+    viewport: { width: 1920, height: 1080 },
+    ignoreHTTPSErrors: true,
+    video: "off",
+    screenshot: "only-on-failure",
+  },
+};
+
+export default config;
